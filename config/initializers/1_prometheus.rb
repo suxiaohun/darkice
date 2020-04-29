f384955768d56db6382a4f3bcfc659766415530d
suxiaohun <suxiaohun@test.com>
2020-04-29 17:44:15 +0800

    Attempt monit requests use prometheus

---
+++ config/initializers/1_prometheus.rb
@@ -0,0 +1,8 @@
+#if Rails.env != "test"
+#  require 'prometheus_exporter/middleware'
+#
+#  # This reports stats per request like HTTP status and timings
+#  Rails.application.middleware.unshift PrometheusExporter::Middleware
+#
+#end
+

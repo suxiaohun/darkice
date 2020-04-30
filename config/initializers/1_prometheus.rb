d38946e1cd861be00df4a8be1a89f2deb49c5b3c
suxiaohun <suxiaohun@test.com>
2020-04-30 11:02:45 +0800

    Fix prometheus issue

--- config/initializers/1_prometheus.rb
+++ config/initializers/1_prometheus.rb
@@ -1,6 +1,29 @@
 unless Rails.env == "test"
-  require 'prometheus_exporter/middleware'
+
+# client allows instrumentation to send info to server
+  require 'prometheus_exporter/client'
   require 'prometheus_exporter/instrumentation'
+  require 'prometheus_exporter/middleware'
+  require 'prometheus_exporter/server'
+
+# bind is the address, on which the webserver will listen
+# port is the port that will provide the /metrics route
+  server = PrometheusExporter::Server::WebServer.new bind: 'localhost', port: 9394
+  server.start
+
+# wire up a default local client
+  PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(collector: server.collector)
+
+  gauge = PrometheusExporter::Metric::Gauge.new("rss", "used RSS for process")
+  counter = PrometheusExporter::Metric::Counter.new("web_requests", "number of web requests")
+  summary = PrometheusExporter::Metric::Summary.new("page_load_time", "time it took to load page")
+  histogram = PrometheusExporter::Metric::Histogram.new("api_access_time", "time it took to call api")
+
+  server.collector.register_metric(gauge)
+  server.collector.register_metric(counter)
+  server.collector.register_metric(summary)
+  server.collector.register_metric(histogram)
+
 
   # this reports basic process stats like RSS and GC info
   PrometheusExporter::Instrumentation::Process.start(type: "master")

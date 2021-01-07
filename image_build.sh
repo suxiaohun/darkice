a70b6d4be576724087e75f157a69cc7d0f20c636
suxiaohun <suxiaohun@test.com>
2021-01-07 13:53:33 +0800

    Split static assets

--- image_build.sh
+++ image_build.sh
@@ -7,20 +7,6 @@ docker build -t  xiaopang:master  .
 docker tag xiaopang:master ccr.ccs.tencentyun.com/crystal/xiaopang:master
 docker push ccr.ccs.tencentyun.com/crystal/xiaopang:master
 
-
-rm -rf public/assets
-rm -rf public/packs
-bundle exec rake assets:precompile
-cp .dockerignore.static .dockerignore
-docker build -t  xiaopang.static:master  -f docker/static.assets  .
-docker tag xiaopang.static:master ccr.ccs.tencentyun.com/crystal/xiaopang.static:master
-docker push ccr.ccs.tencentyun.com/crystal/xiaopang.static:master
-
-# reset the config to local
-cp config/database.yml.default config/database.yml
-cp config/property/property.local.yml config/property.yml
-
-echo "" > .dockerignore
 #docker image prune -f
 docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
 

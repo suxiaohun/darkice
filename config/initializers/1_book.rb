a6c36a03e698a48d5cd1a05fcb45061676920afa
suxiaohun <suxiaohun@test.com>
2021-01-15 10:44:59 +0800

    Image build shell reset config files

--- config/initializers/1_book.rb
+++ config/initializers/1_book.rb
@@ -1,7 +1,5 @@
-BOOK_DEFAULT_LINES = 20
-if Rails.env.development?
-  BOOK_PATH_PREFIX = 'public/txt2/'
-else
-  BOOK_PATH_PREFIX = 'public/books/'
-end
+BOOK_DEFAULT_LINES = 40
+
+BOOK_PATH_PREFIX = 'public/books/'
+
 
--- image_build.sh
+++ image_build.sh
@@ -7,6 +7,10 @@ docker build -t  xiaopang:master  .
 docker tag xiaopang:master ccr.ccs.tencentyun.com/crystal/xiaopang:master
 docker push ccr.ccs.tencentyun.com/crystal/xiaopang:master
 
+# reset the config to local
+cp config/database.yml.default config/database.yml
+cp config/property/property.local.yml config/property.yml
+
 #docker image prune -f
 docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
 

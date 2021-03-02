4095682931580c731445aae03946e0375c9d500c
suxiaohun <suxiaohun@test.com>
2021-03-02 14:43:12 +0800

    Remove nsq producer when server start

--- config/initializers/nsq.rb
+++ config/initializers/nsq.rb
@@ -1,5 +1,6 @@
 require 'nsq'
-NSQ_PRODUCER = Nsq::Producer.new(
-  nsqd: '10.4.5.13:4150',
-  topic: 'test'
-)
+
+# NSQ_PRODUCER = Nsq::Producer.new(
+#   nsqd: '10.4.5.13:4150',
+#   topic: 'test'
+# )

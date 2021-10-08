ab3c243351f339827b06fce0d8f8917b9e0208fd
suxiaohun <suxiaohun@test.com>
2021-10-08 19:37:37 +0800

    Fix migration error

--- db/migrate/20211008093534_create_users.rb
+++ db/migrate/20211008093534_create_users.rb
@@ -4,11 +4,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :uuid, limit: 32, null: false
       t.string :password_digest, null: false
-      t.string :name, null: false
       t.integer :role, null: false
       t.integer :state, null: false
-
-      t.timestamps
 
       t.timestamps
     end

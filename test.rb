d3dae44d825eea56e00bc42eb0d74d2fe54e398e
suxiaohun <suxiaohun666@gmail.com>
2022-03-05 17:22:00 +0100

    Integer to Roman

--- test.rb
+++ test.rb
@@ -17,3 +17,35 @@ def convert(s, num_rows)
   end
   rows.values.join
 end
+
+# @param {Integer} num
+# @return {String}
+def int_to_roman(num)
+  g = num % 10
+  s = num % 100 / 10
+  b = num % 1000 / 100
+  q = num / 1000
+
+  qs = "".rjust(q, "M")
+  bs = spec(b,"C","D","M")
+  ss = spec(s,"X","L","C")
+  gs = spec(g,"I","V","X")
+  qs + bs + ss + gs
+end
+
+def spec(n,s1,s5,s9)
+  case n
+  when 1,2,3
+    "".rjust(n,s1)
+  when 4
+    s1+s5
+  when 5
+    s5
+  when 6,7,8
+    s5+"".rjust(n%5,s1)
+  when 9
+    s1+s9
+  else
+    ""
+  end
+end

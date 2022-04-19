9f5a7cb9d5006b41db24621018300665505a85b8
suxiaohun <suxiaohun666@gmail.com>
2022-04-19 14:16:29 +0800

    Use puma replace unicorn as http server

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ RUN bundle install
 
 COPY . .
 
-CMD ["bundle","exec", "unicorn","-c","config/unicorn.rb","-E","production"]
+CMD ["rails", "s", "-e", "production"]
--- config/puma.rb
+++ config/puma.rb
@@ -8,9 +8,6 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-
-
-bind 'tcp://0.0.0.0:3000'
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }

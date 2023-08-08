77eb19d617c1ac40e78644b60030b90acdd69c1a
huxiaoyang <huxiaoyang@sensetime.com>
2023-08-08 18:02:19 +0800

    K8s use containerd as default cr now

---
+++ lib/utils/containerd.rb
@@ -0,0 +1,8 @@
+# k8s use containerd as the default container runtime, so should import images by ctr, and manage images by crictl
+
+
+
+
+# ctr images import xxx or crictl pull xxx
+# crictl images
+

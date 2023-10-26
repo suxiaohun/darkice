53a82915436056543f733c58259d90d31a3b80e2
suxiaohun <h13616489001@gmail.com>
2023-10-26 17:19:49 +0800

    Fix post css style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :authenticate_user!, only: %i[ create new edit update destroy ]
 
 
   # POST /comments or /comments.json
   def create
     comment = @post.comments.create! params.required(:comment).permit(:content)
+    comment.created_by = current_user.id
+    comment.created_by_name = current_user.name
+    comment.save!
     # CommentsMailer.submitted(comment).deliver_later
     redirect_to @post
   end

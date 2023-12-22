e1a02f50e1ba058622463023159ddf76dfcd990f
suxiaohun <h13616489001@gmail.com>
2023-12-22 15:43:33 +0800

    Fix file type wrong

--- config/initializers/2_sidekiq.rb
+++ config/initializers/2_sidekiq.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Sidekiq.configure_server do |config|
   config.redis = {namespace: 'xiaopang', size: 25, url: "redis://#{XiaopangEnv.redis.host}:#{XiaopangEnv.redis.port}/#{XiaopangEnv.redis.db}"}
 end

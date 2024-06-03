8c7c352fb3a78984d50eb23818c90a64a53921d2
suxiaohun <h13616489001@gmail.com>
2024-06-03 16:14:02 +0800

    Use class variable for colors save

--- lib/utils/rgb_color.rb
+++ lib/utils/rgb_color.rb
@@ -1,37 +1,29 @@
 class RgbColor
+  @@colors = []
+
   class << self
 
     def colors
-      colors = []
-      File.open("#{Rails.root.to_s}/lib/utils/files/rgb_color.txt").each_line do |line|
-        colors << line.split
+      if @@colors.empty?
+        File.open("#{Rails.root.to_s}/lib/utils/files/rgb_color.txt").each_line do |line|
+          @@colors << line.split
+        end
       end
-      colors
+      @@colors
     end
-
 
     def rand_color
-      @rand_color = colors[rand(colors.length)][1]
+      @rand_color = @@colors[rand(@@colors.length)][1]
     end
 
+    # light color
     def fav_color
       fav_colors = %w(
-                  #FF8C00
-                  #DB7093
-                  #DB7093
-                  #D2B48C
-                  #EEE8AA
-                  #F5DEB3
-                  #EE82EE
-                  #00FA9A
-                  #FFD700
-                  #DAA520
-                  #ADFF2F
+                  #FF8C00 #DB7093 #DB7093 #D2B48C #EEE8AA #F5DEB3 #EE82EE
+                  #00FA9A #FFD700 #DAA520 #ADFF2F #5F9EA0 #98FB98 #FFFFE0
                )
       fav_colors[rand(fav_colors.length)]
     end
-
-
 
     def lavender
       "#E6E6FA"
@@ -40,7 +32,6 @@ class RgbColor
     def silver
       "#C0C0C0"
     end
-
 
   end
 

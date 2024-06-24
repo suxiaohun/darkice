529e0565d217bfde36679ca048496edc45943ba7
suxiaohun <h13616489001@gmail.com>
2024-06-24 15:45:00 +0800

    Add reading history

---
+++ app/javascript/books/books.js
@@ -0,0 +1,13 @@
+document.addEventListener('turbo:load', function(event) {
+    var toggleButton = document.getElementById('history-btn');
+    var toggleDiv = document.getElementById('history-div');
+
+    toggleButton.addEventListener('click', function(){
+        var currDisplay = window.getComputedStyle(toggleDiv).display
+        if (currDisplay === 'none') {
+            toggleDiv.style.display = 'block';
+        }else{
+            toggleDiv.style.display = 'none';
+        }
+    })
+})

9f51f197317a903138a9bf27e0d9252a38f17c6b
suxiaohun <h13616489001@gmail.com>
2024-09-29 10:54:47 +0800

    Emberd iframe for grafana dashboard

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Darkice
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
